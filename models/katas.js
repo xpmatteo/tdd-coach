a085e7e160512112c2f8f3812356da396bee8bdd
Matteo Vaccari <mvaccari@thoughtworks.com>
2025-04-13 18:26:01 +0200

    Improved prompt for "red" phase

--- models/katas.js
+++ models/katas.js
@@ -7,7 +7,14 @@ module.exports = {
     name: 'FizzBuzz',
     description: 'Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".',
     initialProductionCode: 'function fizzBuzz(number) {\n  // Implement FizzBuzz here\n}\n\nmodule.exports = { fizzBuzz };',
-    initialTestCode: 'const { fizzBuzz } = require("./fizzbuzz");\n\ndescribe("FizzBuzz", () => {\n  // Write your tests here\n});',
+    initialTestCode: `
+    const { fizzBuzz } = require("./fizzbuzz");
+    
+    describe("FizzBuzz", () => {
+      test("sample test -- replace with your own", () => {
+        expect(1).toBe(1);
+      });
+    });`,
     testCases: [
       {
         id: 1,
